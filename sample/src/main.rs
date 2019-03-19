859af63171749d6b44bb399358ef543a10b73656
hamadakafu <kafu.h1998@gmail.com>
2019-03-19 23:55:43 +0900

    test kms

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,3 +1,13 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
 fn main() {
-    println!("{}", "Heloo world");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("0.0.0.0:8000")
+        .unwrap()
+        .run();
 }
