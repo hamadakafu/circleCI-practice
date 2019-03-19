9f0cd0ae92532b87a2536c1c8b879d5ed1606cca
hamadakafu <kafu.h1998@gmail.com>
2019-03-19 12:17:16 +0900

    test deployment: debug1: give service-account authorit: debug2: fixed apply -f

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,24 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-mod some {
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        #[test]
-        fn test_add_one() {
-            assert_eq!(3, add_one(2));
-        }
-    }
-    fn add_one(x: isize) -> isize {
-        x + 1
-    }
-}
-use rocket::{routes, get};
-#[get("/")]
-fn hello() -> String {
-    "hello, be built".to_string()
-}
 fn main() {
-    rocket::ignite().mount("/", routes![
-        hello,
-    ]).launch();
+    println!("{}", "Heloo world");
 }
