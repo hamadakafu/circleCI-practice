99fc6c3b9a051f4c1a33db0caf40515c1d0d3d6f
hamadakafu <kafu.h1998@gmail.com>
2019-03-20 13:16:44 +0900

    deployment test25

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -6,6 +6,10 @@ use rocket::{get, routes};
 fn hello() -> String {
     "hello".to_string()
 }
+#[get("/<name>/<age>")]
+fn name_age(name: String, age: usize) -> String {
+    format!("hello {}. age: {}", name, age)
+}
 
 fn main() {
     rocket::ignite().mount("/", routes![hello]).launch();
